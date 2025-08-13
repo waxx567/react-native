89e303feb0198f2d1495f860cbaf6b71b366ce30
Wayne <waynem567@gmail.com>
2025-08-13 12:16:20 +0200

    Test link again

--- mobile-movie-app/app/(tabs)/index.tsx
+++ mobile-movie-app/app/(tabs)/index.tsx
@@ -1,7 +1,6 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -18,7 +17,6 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome to my Movie App!</ThemedText>
-        <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
